b17a901aaec350da7e8d855c57afe341371aa183
marshall552 <laedismarshall@gmail.com>
2025-05-02 09:23:54 +0800

    final commit

--- routes/web.php
+++ routes/web.php
@@ -13,38 +13,28 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+
     Route::get('/members', [MemberController::class, 'index'])->middleware('role:admin')->name('members');
     Route::post('/members', [MemberController::class, 'store'])->middleware('role:admin')->name('members.store');
     Route::patch('/members/{user}', [MemberController::class, 'update'])->middleware('role:admin')->name('members.update');
     Route::delete('/members/{user}', [MemberController::class, 'destroy'])->middleware('role:admin')->name('members.destroy');
+
     Route::get('calendar', function () {
-        return Inertia::render('Calendar');
+        return Inertia::render('calendar'); // Note: Use 'Calendar' (capitalized, no .tsx extension)
     })->name('calendar');
+
     Route::get('notifications', [NotificationController::class, 'index'])->name('notifications');
     Route::post('notifications/mark-all-read', [NotificationController::class, 'markAllRead'])->name('notifications.markAllRead');
     Route::post('notifications/{notification}/mark-read', [NotificationController::class, 'markAsRead'])->name('notifications.markAsRead');
+
     Route::get('assignee', [TaskController::class, 'index'])->name('assignee.index');
     Route::post('assignee', [TaskController::class, 'store'])->name('assignee.store');
     Route::patch('assignee/{task}', [TaskController::class, 'update'])->name('assignee.update');
     Route::delete('assignee/{task}', [TaskController::class, 'destroy'])->name('assignee.destroy');
+
 });
 
-// User Routes
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/user-landing', function () {
-        return Inertia::render('User/UserLandingPage');
-    })->name('user.landing');
-    Route::get('/user/dashboard', function () {
-        return Inertia::render('User/UserDashboard');
-    })->name('user.dashboard');
-    Route::get('/user/notification', function () {
-        return Inertia::render('User/UserNotification');
-    })->name('user.tasks');
-    Route::get('/user/tasks', function () {
-        return Inertia::render('User/UserTask');
-    })->name('user.tasks');
-});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
